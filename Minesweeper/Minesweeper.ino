c427b22c71f3ce063367f597b2a1a3fea9477b2e
Dirk Steindorf <dirk.steindorf@gmail.com>
2015-01-17 15:10:03 +0100

    the flag isn't a crooked 1 anymore

--- Minesweeper/Minesweeper.ino
+++ Minesweeper/Minesweeper.ino
@@ -66,12 +66,14 @@ Game_state game_state;
 //define special characters (flag, neutral face)
 const byte flag[] PROGMEM=
 {
-	8, 5,
-	B01100000,
-	B11100000,
-	B10100000,
-	B00100000,
-	B00100000,
+    8, 7,
+    B10000000,
+    B11000000,
+    B11100000,
+    B10100000,
+    B10000000,
+    B10000000,
+    B00000000,
 };
 
 const byte neutral[] PROGMEM=
